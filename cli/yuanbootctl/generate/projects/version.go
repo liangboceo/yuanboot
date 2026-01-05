e45b4424fd69c47693cf276e37bbdcf0b66b18e0
liangbo <173120209@qq.com>
2026-01-05 18:48:31 +0800

    add:修复日志问题

--- cli/yuanbootctl/generate/projects/version.go
+++ cli/yuanbootctl/generate/projects/version.go
@@ -2,7 +2,7 @@ package projects
 
 const (
 	//Application Version, such as v1.x.x pre-release
-	Version = "v1.9.25"
+	Version = "v1.9.26"
 	//Application logo
 	Logo = `                                          █████                        █████   
                                            ░░███                        ░░███    
