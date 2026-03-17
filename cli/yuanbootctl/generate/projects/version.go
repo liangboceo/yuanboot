c7fe37bad00c19fb286cc01a67b019ce1a222c5f
liangbo <173120209@qq.com>
2026-03-17 18:24:07 +0800

    update:redis增加发布订阅逻辑，支持异步发布订阅场景

--- cli/yuanbootctl/generate/projects/version.go
+++ cli/yuanbootctl/generate/projects/version.go
@@ -2,7 +2,7 @@ package projects
 
 const (
 	//Application Version, such as v1.x.x pre-release
-	Version = "v1.9.268"
+	Version = "v1.9.269"
 	//Application logo
 	Logo = `                                          █████                        █████   
                                            ░░███                        ░░███    
