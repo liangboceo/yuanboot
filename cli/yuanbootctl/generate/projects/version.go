c6bec80541d3dfd219c70903e01a342763d06463
liangbo <173120209@qq.com>
2026-03-19 23:09:54 +0800

    update:redisson实现

--- cli/yuanbootctl/generate/projects/version.go
+++ cli/yuanbootctl/generate/projects/version.go
@@ -2,7 +2,7 @@ package projects
 
 const (
 	//Application Version, such as v1.x.x pre-release
-	Version = "v1.9.270"
+	Version = "v1.9.272"
 	//Application logo
 	Logo = `                                          █████                        █████   
                                            ░░███                        ░░███    
