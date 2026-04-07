18765eb208c0c6d966be1f053c13b5b9e2a5edd1
liangbo <173120209@qq.com>
2026-04-07 14:25:42 +0800

    update:升级到最新的redis框架，提升性能

--- cli/yuanbootctl/generate/projects/version.go
+++ cli/yuanbootctl/generate/projects/version.go
@@ -2,7 +2,7 @@ package projects
 
 const (
 	//Application Version, such as v1.x.x pre-release
-	Version = "v1.9.2741"
+	Version = "v1.9.2742"
 	//Application logo
 	Logo = `                                          █████                        █████   
                                            ░░███                        ░░███    
