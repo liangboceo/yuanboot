755085b6dd1a97c5e21618fcbd7e3abd78edfb73
liangbo <173120209@qq.com>
2026-05-27 18:08:03 +0800

    update:修复路由查询的bug

--- cli/yuanbootctl/generate/projects/version.go
+++ cli/yuanbootctl/generate/projects/version.go
@@ -2,7 +2,7 @@ package projects
 
 const (
 	//Application Version, such as v1.x.x pre-release
-	Version = "v1.9.2750"
+	Version = "v1.9.2751"
 	//Application logo
 	Logo = `                                          █████                        █████   
                                            ░░███                        ░░███    
