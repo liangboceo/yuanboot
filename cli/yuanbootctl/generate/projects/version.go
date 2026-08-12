0653524c5530cd444c5304294278ac128973df31
liangbo <173120209@qq.com>
2026-08-12 16:59:34 +0800

    update:增加生成前后端脚手架

--- cli/yuanbootctl/generate/projects/version.go
+++ cli/yuanbootctl/generate/projects/version.go
@@ -2,7 +2,7 @@ package projects
 
 const (
 	//Application Version, such as v1.x.x pre-release
-	Version = "v1.9.2804"
+	Version = "v1.9.2805"
 	//Application logo
 	Logo = `                                          █████                        █████   
                                            ░░███                        ░░███    
